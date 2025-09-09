aef3fef758d2b54458f1ac5b89ca5f5c0b016be7
TIAGO HENRIQUE SILVA DE CARVALHO <tiago.carvalho@crespoecaires.com.br>
2025-09-08 22:34:54 -0300

    Atualização Font JobOrder e adição dockerFile

--- HubWalks/Controllers/JobOrdersController.cs
+++ HubWalks/Controllers/JobOrdersController.cs
@@ -28,17 +28,12 @@ namespace HubWalks.Controllers
         // GET: JobOrders/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var jobOrder = await _context.OrdensDeServico
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (jobOrder == null)
-            {
-                return NotFound();
-            }
+
+            if (jobOrder == null) return NotFound();
 
             return View(jobOrder);
         }
@@ -46,12 +41,21 @@ namespace HubWalks.Controllers
         // GET: JobOrders/Create
         public IActionResult Create()
         {
+            var clientes = _context.Clientes.ToList();
+            var sdrBdrs = _context.Sdr_Bdrs.ToList();
+
+            if (!clientes.Any() || !sdrBdrs.Any())
+            {
+                ViewBag.SemClientesOuBdr = true;
+            }
+
+            ViewBag.Clientes = new SelectList(clientes, "IdCliente", "NomeCliente");
+            ViewBag.SdrBdrs = new SelectList(sdrBdrs, "IdSdr_Bdr", "Nome");
+
             return View();
         }
 
         // POST: JobOrders/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,NomeProjeto,Descricao,DataSolicitacao,IdClient,Closer,Sdr_Bdr,PercentualComissaoComercial,Observacao,Prazo,Promessas,Valor,FormaPagamento")] JobOrder jobOrder)
@@ -62,36 +66,34 @@ namespace HubWalks.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
+            // Recarregar selects em caso de erro de validação
+            ViewBag.Clientes = new SelectList(_context.Clientes, "IdCliente", "NomeCliente", jobOrder.IdClient);
+            ViewBag.SdrBdrs = new SelectList(_context.Sdr_Bdrs, "IdSdr_Bdr", "Nome", jobOrder.Sdr_Bdr);
+
             return View(jobOrder);
         }
 
         // GET: JobOrders/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var jobOrder = await _context.OrdensDeServico.FindAsync(id);
-            if (jobOrder == null)
-            {
-                return NotFound();
-            }
+            if (jobOrder == null) return NotFound();
+
+            ViewBag.Clientes = new SelectList(_context.Clientes, "IdCliente", "NomeCliente", jobOrder.IdClient);
+            ViewBag.SdrBdrs = new SelectList(_context.Sdr_Bdrs, "IdSdr_Bdr", "Nome", jobOrder.Sdr_Bdr);
+
             return View(jobOrder);
         }
 
         // POST: JobOrders/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,NomeProjeto,Descricao,DataSolicitacao,IdClient,Closer,Sdr_Bdr,PercentualComissaoComercial,Observacao,Prazo,Promessas,Valor,FormaPagamento")] JobOrder jobOrder)
         {
-            if (id != jobOrder.Id)
-            {
-                return NotFound();
-            }
+            if (id != jobOrder.Id) return NotFound();
 
             if (ModelState.IsValid)
             {
@@ -103,33 +105,28 @@ namespace HubWalks.Controllers
                 catch (DbUpdateConcurrencyException)
                 {
                     if (!JobOrderExists(jobOrder.Id))
-                    {
                         return NotFound();
-                    }
                     else
-                    {
                         throw;
-                    }
                 }
                 return RedirectToAction(nameof(Index));
             }
+
+            ViewBag.Clientes = new SelectList(_context.Clientes, "IdCliente", "NomeCliente", jobOrder.IdClient);
+            ViewBag.SdrBdrs = new SelectList(_context.Sdr_Bdrs, "IdSdr_Bdr", "Nome", jobOrder.Sdr_Bdr);
+
             return View(jobOrder);
         }
 
         // GET: JobOrders/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var jobOrder = await _context.OrdensDeServico
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (jobOrder == null)
-            {
-                return NotFound();
-            }
+
+            if (jobOrder == null) return NotFound();
 
             return View(jobOrder);
         }
