e486251f8462b8efab5abe81dc7413bf56615eba
TIAGO HENRIQUE SILVA DE CARVALHO <tiago.carvalho@crespoecaires.com.br>
2025-09-09 10:26:46 -0300

    Removido criação de roles dafault para teste

--- HubWalks.Data/Context/HubWalksDbContext.cs
+++ HubWalks.Data/Context/HubWalksDbContext.cs
@@ -29,10 +29,10 @@ namespace HubWalks.Data.Context
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<IdentityRole>().HasData(
-                new IdentityRole { Id = "role-admin", Name = "Admin", NormalizedName = "ADMIN", ConcurrencyStamp = Guid.NewGuid().ToString() },
-                new IdentityRole { Id = "role-user", Name = "User", NormalizedName = "USER", ConcurrencyStamp = Guid.NewGuid().ToString() }
-            );
+            //builder.Entity<IdentityRole>().HasData(
+            //    new IdentityRole { Id = "role-admin", Name = "Admin", NormalizedName = "ADMIN", ConcurrencyStamp = Guid.NewGuid().ToString() },
+            //    new IdentityRole { Id = "role-user", Name = "User", NormalizedName = "USER", ConcurrencyStamp = Guid.NewGuid().ToString() }
+            //);
         }
     }
 }
